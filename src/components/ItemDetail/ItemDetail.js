37dfeb35347a3fb734725669b9e2343c89a68bca
CesarTG <cesartorres94t@gmail.com>
2022-06-21 20:36:53 -0300

    responsive

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,11 +1,12 @@
 import React from "react";
+import '../ItemDetail/ItemDetail.css'
 
 export const ItemDetail = ({detalle}) => {
 
-    return (<div>
-        <h2>{detalle.nombre}</h2>
-        <img src={detalle.imagen} alt="Botella de whisky DOBLE-V"/>
-        <p>{detalle.descripcion}</p>        
+    return (<div className="item-contDetail">
+        <h2 className="h2-ID">{detalle.nombre}</h2>
+        <img className="img-ID" src={detalle.imagen} alt="Botella de whisky DOBLE-V"/>
+        <p className="p-itemDetail">{detalle.descripcion}</p>        
     </div>
 
     );
