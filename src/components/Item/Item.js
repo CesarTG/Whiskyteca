e5fa9f4f36a6ed317a4d2d2c89ca4544b161456f
CesarTG <cesartorres94t@gmail.com>
2022-06-20 22:38:54 -0300

    etiquetas semanticas

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,7 +3,7 @@ import React from "react";
 function Item ({id, nombre ,role }) {
     return <div>
         <h4>{id}</h4>
-        <h4>{nombre}</h4>
+        <h3>{nombre}</h3>
         <h3>{role}</h3>
         <button>Tomar 1 Rock glass</button>        
     </div>;     
