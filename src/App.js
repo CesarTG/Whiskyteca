e3e94ab264a37c399ebd2392168052c50ed2f2fa
CesarTG <cesartorres94t@gmail.com>
2022-06-30 23:27:43 -0300

    context

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import { NavBar } from "./components/NavBar/NavBar"
 import { ItemListContainer } from './components/ItemListContainer/ItemListContainer';
 import { ItemDetailContainer } from './components/ItemDetailContainer/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
 
+export const carritoContext = React.createContext('')
+console.log(carritoContext)
 
 function App() {
 
   return <>
     <BrowserRouter>
       <NavBar />
-      <Routes>
+      <carritoContext.Provider>
+        <Routes>
         <Route path='/' element={<ItemListContainer />} />        
         <Route path='/whiskys/:id' element={<ItemDetailContainer />} />
         <Route path='/whiskys' element={<ItemListContainer />} />            
       </Routes>
+      </carritoContext.Provider>      
     </BrowserRouter>
   </>;
 }
