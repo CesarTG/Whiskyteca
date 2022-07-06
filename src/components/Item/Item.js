67c4eeb05612163f21c6afec7f2b4dcd6cb56f4f
CesarTG <cesartorres94t@gmail.com>
2022-07-05 21:22:27 -0300

    detalles en los productos

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -12,8 +12,7 @@ function Item({ id, nombre, role, imagen, sabor }) {
     return <div className="item-container">
         <h4 className="id">ID: {id}</h4>
         <h3 className="name">{nombre}</h3>
-        <h3 className="role">Sabor: {role}</h3>
-        <h3>flavor: {sabor}</h3>
+        <h3 className="role">Sabe a: {role}</h3>       
         <img className="imagen" src={imagen} alt="Botella "/>
         <Link className="linkItem" to={`/whiskys/${id}`}> Tomar un RockGlass! </Link>
     </div>
--- src/utils/productos.js
+++ src/utils/productos.js
@@ -3,42 +3,42 @@ export const productos = [
     {
         id: "1",
         nombre: "Doble-V",
-        role: "Suave",
+        role: "Aceituna ácida",
         imagen: "https://http2.mlstatic.com/D_NQ_NP_833044-MLA30548552299_052019-O.jpg",
         sabor:"venenoso"
     },
     {
         id: "2",
         nombre: "Criadores",
-        role: "Fuerte",
+        role: "Tercer mundismo",
         imagen: "https://elparaisodelvinardo.com.ar/wp-content/uploads/2021/07/CRIADORES.jpg",
         sabor:"venenoso"
     },
     {
         id: "3",
         nombre: "Tocornal",
-        role: "Muy suave",
+        role: "Que hace un vino aqui?",
         imagen: "https://www.grupoelnene.com.ar/4176/vino-tocornal-tinto-1125cc.jpg",
         sabor:"suave"
     },
     {
         id: "4",
         nombre: "Blenders Pride",
-        role: "whisky4",
+        role: "Vinagreta añeja",
         imagen: "https://gpalmioli.typepad.com/.a/6a00d834674fbc53ef00e553a9f0fe8833-pi",
         sabor:"fuerte"
     },
     {
         id: "5",
         nombre: "Old Smuggler",
-        role: "whisky5",
+        role: "Derrota",
         imagen: "https://upload.wikimedia.org/wikipedia/commons/5/59/Old_Smuggler_Scotch_whisky.jpg",
         sabor:"fuerte"
     }, 
     {
         id: "6",
         nombre: "Jameson",
-        role: "whisky6",
+        role: "James...Bond",
         imagen: "https://http2.mlstatic.com/D_NQ_NP_958253-MLA46846414268_072021-O.webp",
         sabor:"suave"
     }
